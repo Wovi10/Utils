456765a2c1e9fcb0027617e85b26cbafc6e08c1e
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-27 16:40:20 +1100

    Started utils package

---
+++ src/main/java/Exceptions/NoIntegerException.java
@@ -0,0 +1,45 @@
+package Exceptions;
+
+import java.security.PrivilegedActionException;
+
+public class NoIntegerException extends Exception{
+    private static final String message = "The given variable was not an Integer";
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public NoIntegerException() {
+        System.err.println(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail customMessage.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param customMessage the detail customMessage. The detail customMessage is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public NoIntegerException(String customMessage) {
+        super(customMessage);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of {@code (cause==null ? null : cause.toString())} (which
+     * typically contains the class and detail message of {@code cause}).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * PrivilegedActionException}).
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A {@code null} value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     * @since 1.4
+     */
+    public NoIntegerException(Throwable cause) {
+        super(message, cause);
+    }
+}
