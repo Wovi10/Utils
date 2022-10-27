b712ae8f155bbb126c39304725dbc2948157adfd
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-27 16:55:13 +1100

    Added JavaDoc

--- src/main/java/Debug/DebugHelp.java
+++ src/main/java/Debug/DebugHelp.java
@@ -8,6 +8,11 @@ public class DebugHelp {
     public static void systemPrint_WriteTo(String fileLocation){
         System.out.printf("Writing to %s: ", fileLocation);
     }
+
+    /**
+     * Writes to the System.out from which file is being read.
+     * @param fileLocation The location to be shown in System.out
+     */
     public static void systemPrint_ReadFrom(String fileLocation){
         System.out.printf("Reading from %s: ", fileLocation);
     }
